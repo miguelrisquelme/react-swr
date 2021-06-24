8c9c58bd0e49899da1a925fe82565309b5c3d00e
miguelrisquelme <miguelrisquelme@gmail.com>
2021-06-23 21:23:49 -0300

    changes with swr

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -1,31 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
-// import { Container } from './styles';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 interface User {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 const UserDetails: React.FC = () => {
-  const { id } = useParams();
-  const [data, setData] = useState<User>()
+	const { id } = useParams();
+	const { data } = useFetch<User>(`/users/${id}`);
 
-  useEffect(() => {
-    fetch(`http://localhost:3333/users/${id}`).then(response => {
-      response.json().then(user => {
-        setData(user);
-      })
-    })
-  }, [id]);
+	if (!data) return <p>Carregando...</p>;
 
-  return (
-    <ul>
-      <li>ID: {data?.id}</li>
-      <li>Name: {data?.name}</li>
-    </ul>
-  );
-}
+	return (
+		<ul>
+			<li>ID: {data?.id}</li>
+			<li>Name: {data?.name}</li>
+		</ul>
+	);
+};
 
 export default UserDetails;
--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,33 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback } from "react";
+import { Link } from "react-router-dom";
+import { mutate as mutateGlobal } from "swr";
+
+import { useFetch } from "../hooks/useFetch";
+import api from "../services/api";
 
 interface User {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 const UserList: React.FC = () => {
-  const [data, setData] = useState<User[]>([]);
+	const { data, mutate } = useFetch<User[]>("/users");
 
-  useEffect(() => {
-    fetch('http://localhost:3333/users').then(response => {
-      response.json().then(users => {
-        setData(users);
-      });
-    })
-  }, []);
+	const handleNameChange = useCallback((id: number) => {
+		api.put(`/users/${id}`, { name: "Novo nome" });
 
-  return (
-    <ul>
-      {data.map(user => (
-        <li key={user.id}>
-          <Link to={`/users/${user.id}`}>
-            {user.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-}
+		const updatedUsers = data?.map((user) => {
+			if (user.id === id) return { ...user, name: "Bartolomeu" };
+			return user;
+		});
+
+		mutate(updatedUsers, false);
+		mutateGlobal(`/users/${id}`, { id, name: "Bartolomeu" });
+	}, []);
+
+	if (!data) return <p>Carregando...</p>;
+
+	return (
+		<ul>
+			{data?.map((user) => (
+				<li key={user.id}>
+					<Link to={`/users/${user.id}`}>{user.name}</Link>
+					<button type="button" onClick={() => handleNameChange}>
+						Alterar nome
+					</button>
+				</li>
+			))}
+		</ul>
+	);
+};
 
 export default UserList;
